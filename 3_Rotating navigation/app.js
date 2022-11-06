94df53c56f7c15e931902d9d411c3c635733c603
SashR <sashinramdhani@gmail.com>
2022-11-06 14:20:28 +0200

    Complete rotating navigation

---
+++ 3_Rotating navigation/app.js
@@ -0,0 +1,12 @@
+console.log(document.querySelector('#open'));
+
+const openButton = document.querySelector('#open');
+const closeButton = document.querySelector('#close');
+const container = document.querySelector('.container');
+
+openButton.addEventListener('click', ()=>{
+    container.classList.add('show-nav');
+});
+closeButton.addEventListener('click', ()=>{
+    container.classList.remove('show-nav');
+});
